ec64bd50f2344c3300c9576e66ae4ae3f071eeea
권윤하 <jenga@gwon-yunhaui-MacBook-Pro.local>
2019-04-15 16:35:45 +0900

    day1

---
+++ day1.js
@@ -0,0 +1,12 @@
+function twoSum(nums, target) {
+
+  for(i=0; i<nums.length; i++){
+    for(j=0; j<nums.length; j++){
+      if(nums[i] + nums[j] === target){
+        return [i, j];
+      }
+    }
+  }
+}
+
+
